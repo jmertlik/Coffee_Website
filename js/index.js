e591b4832371fad88feb41fdb0dbf6b671f7fbe7
Jan Mertlik <jm8801@mun.ca>
2020-03-11 11:21:08 -0230

    javascript play

---
+++ js/index.js
@@ -0,0 +1,28 @@
+console.log("Hello!");
+
+let courseTitle = "Intro to web development";
+let courseLength = "8 weeks";
+
+console.log(courseTitle);
+console.log(courseLength);
+
+let courseAttendees = ["Anskey", "Terri", "Kat", "Annagray"];
+console.log(courseAttendees);
+console.log(courseAttendees[0]);
+
+courseAttendees[0] = "Haris";
+courseAttendees[4] = "Martin";
+
+let course = {
+    title: "Intro to data science",
+    duration: "8 weeks",
+    attendees: ["Anskey", "Terri", "Kat", "Annagray"]
+}
+
+console.log(course);
+console.log(course.title);
+
+course.instructor = "Jan";
+course.attendees[4] = "Christopher";
+
+
